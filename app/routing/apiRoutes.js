6b4a0d5fd260da47bbe79f31797ed6b117bf9341
sally <sallyakrum83@gmail.com>
2019-03-11 18:34:26 -0500

    pushing the last changing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,3 @@
-
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // ===============================================================================
@@ -8,21 +7,21 @@ var friends = require("../data/friends");
 // ===============================================================================
 
 module.exports = function(app) {
-app.get("/api/friends", function(req, res) {
-     res.json(friends);
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
   });
 
   app.post("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newSurvey = req.body;
-  
-    new.routeSurvey = newSurvey.name.replace(/\s+/g, "").toLowerCase();
-  
+
+    // new.routeSurvey = newSurvey.name.replace(/\s+/g, "").toLowerCase();
+
     console.log(newSurvey);
-  
+
     friends.push(newSurvey);
-  
+
     res.json(newSurvey);
   });
-}
+};
